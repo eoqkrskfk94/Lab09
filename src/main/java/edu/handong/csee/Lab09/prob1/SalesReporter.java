375f483a0e5af225a136d11e84491669225e6752
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-04-13 21:07:20 +0900

    Update using array list

--- src/main/java/edu/handong/csee/Lab09/prob1/SalesReporter.java
+++ src/main/java/edu/handong/csee/Lab09/prob1/SalesReporter.java
@@ -1,5 +1,6 @@
 package edu.handong.csee.Lab09.prob1;
-import java.util.Scanner;
+import java.util.Scanner; //import Scanner class
+import java.util.ArrayList; //import ArrayList class
 /**
  * This class has the follwing methods:<br>
  * SalesReporter constructor<br>
@@ -16,15 +17,9 @@ public class SalesReporter {
 	
 	private double highestSales; //initiates private double variable
 	private double averageSales; //initiates private double variable
-	private Salesman[] team;//initiates private array variable
-	private int numOfSalesman; //initiates private int variable
+	public ArrayList<Salesman> team = new ArrayList<Salesman>(); //initiate an array list public team
+	int i = 0;
 	
-	public SalesReporter() //This method lets the user to enter an input
-	{
-		System.out.print("Enter number of salesman: "); //prints out the information
-		Scanner myScanner = new Scanner(System.in); //creates scanner for insertion
-		numOfSalesman = myScanner.nextInt(); //inserts the input value to numOfSalesman
-	}
 	
 	public void printOutMySentence() //This method prints out the information
 	{
@@ -33,13 +28,14 @@ public class SalesReporter {
 	
 	public void getData() //This method gets the data from the user and inserts them to the variables
 	{
-		team = new Salesman[numOfSalesman]; //declares a salesman array team with the number of the salesman the user inserted
+		team = new ArrayList<Salesman>(); // initiate arraylist team
+		boolean done = true; //initiate boolean done with the value of true
 		
-		for(int i = 0; i < numOfSalesman; i++) //loops until i is smaller than the number of salesman (i starts with 0 and adds 1 every loop)
-		{
-			Salesman salesman = new Salesman(); //declares an array salesman 
+		while(done) { //loop until done is not true
+			Salesman salesman = new Salesman();  //initiate Salesman salesman 
 			Scanner myScanner = new Scanner(System.in); //creates scanner for insertion
 			System.out.println("Enter data for associate number " + (i+1)); //prints out the info
+			i++; //increment i by 1
 			
 			System.out.print("Enter name of sales associate: "); //prints out the info
 			String name = myScanner.nextLine(); //inserts the input value to name
@@ -50,34 +46,44 @@ public class SalesReporter {
 			salesman.setName(name); //inserts the name of salesman using set method
 			salesman.setSales(sales); //inserts the sales of salesman using set method
 			
-			team[i] = salesman; //inserts the info of salesman into i th team
+			team.add(salesman); //add the salesman value to team
 			
+			System.out.print("Do you want to add more associate?");  //prints out the info
+			String buffer = myScanner.nextLine(); //buffer in order to ignore the enter
+			String ans = myScanner.nextLine(); //inserts the input value ans
+			
+			if(!ans.equalsIgnoreCase("yes")) //if ans is not yes then if statement is carried out
+			{
+				done = false; // put false in boolean done
+			}
 			
 		}
+		
+		
 	}
 	
 	public void calculateAverageSales() //this method calculates the average of sales in the team
 	{
+		int listSize = team.size ();
 		double sum = 0; //initiates double sum variable and insert 0 into it
-		for(int i = 0; i < numOfSalesman; i++) //loops until i is smaller than the number of salesman (i starts with 0 and adds 1 every loop)
-		{
-			double sales = team[i].getSales(); //insert the sales value of ith team (using getter method) into sales
-			sum = sum + sales; //add all the sales
+		for(Salesman sales : team) { //for each statement where all the sales in team are called one by one
+			double temp = sales.getSales(); //initiate and set the value of temp with sales.getSales()
+			sum = sum + temp; //add all the sales
 		}
 		
-		averageSales = sum / numOfSalesman; //calculate the average
+		averageSales = sum / listSize; //calculate the average
 	}
 	
 	public void calculateHighestSales() //this method calculates the highest sales of sales in the team
 	{
 		highestSales = 0; //insert 0 to highestSales variable
-		for(int i = 0; i < numOfSalesman - 1; i++) //loops until i is smaller than the number of salesman -1(i starts with 0 and adds 1 every loop)
-		{
-			if(team[i].getSales() < team[i+1].getSales()) //if the value of ith team's sales is smaller than the value of (i+1)team's sales run if statement
-			{
-				highestSales = team[i+1].getSales(); //insert the value to highestSales
+		Double max = Double.MIN_VALUE; //initiate double variable max with min value of double
+		for(Salesman sales : team) { //for each statement where all the sales in team are called one by one
+			if(sales.getSales() > max) { //if statement is carried out if sales.getSales() is greater than max
+				max = sales.getSales(); //insert the value of sales.getSales() into max
 			}
 		}
+		highestSales = max; //insert the value of max into highestSales
 	}
 	
 	public void printOutResults() //prints out the results of the all the calculation
@@ -87,27 +93,27 @@ public class SalesReporter {
 
 		
 		System.out.println("\nThe following had the highest sales:"); //prints out info
-		for(int i = 0; i < numOfSalesman; i++) //loops until i becomes same or bigger than numOfSalesman (i start in the value of 0 and add 1 every loop)
-		{
-			if(team[i].getSales() == highestSales) //if statement is carried out if ith associate's sales is the same with the value of highestSales
-			{
-				System.out.println("Name: " + team[i].getName()); //prints out the name of the associate
-				System.out.println("Sales: $" + team[i].getSales()); //prints out the sales of the associate
+		
+		for(Salesman sales : team) { //for each statement where all the sales in team are called one by one
+			if(sales.getSales() == highestSales) { //if statement is carried out if ith associate's sales is the same with the value of highestSales
+				System.out.println("Name: " + sales.getName()); //prints out the name of the associate
+				System.out.println("Sales: $" + sales.getSales()); //prints out the sales of the associate
 				System.out.println("$" + (highestSales - averageSales) + "above average"); //prints out how much difference there is in highestSales and averageSales
 			}
 		}
 		
+		
 		System.out.println("\nThe rest performed as follows:"); //prints out the info
-		for(int i = 0; i < numOfSalesman; i++) //loops until i becomes same or bigger than numOfSalesman (i start in the value of 0 and add 1 every loop)
-		{
-			if(team[i].getSales() != highestSales) //if statement is carried out if ith associate's sales is not same with the value of highest Sales
-			{
-				System.out.println("Name: " + team[i].getName());//prints out the name of the associate
-				System.out.println("Sales: $" + team[i].getSales());//prints out the sales of the associate
-				if(team[i].getSales() > averageSales) System.out.println("$" + (team[i].getSales() - averageSales) + " above average");
+		
+		
+		for(Salesman sales : team) { //for each statement where all the sales in team are called one by one
+			if(sales.getSales() != highestSales) { //if statement is carried out if ith associate's sales is not same with the value of highest Sales
+				System.out.println("Name: " + sales.getName());//prints out the name of the associate
+				System.out.println("Sales: $" + sales.getSales());//prints out the sales of the associate
+				if(sales.getSales() > averageSales) System.out.println("$" + (sales.getSales() - averageSales) + " above average");
 				//if statement is carried out if ith associate's sales is greater then averageSales
 				//print out how much difference there is in the associate's sales and averageSales
-				else System.out.println("$" + (averageSales - team[i].getSales()) + " below average"); 
+				else System.out.println("$" + (averageSales - sales.getSales()) + " below average"); 
 				//print out how much difference there is in the associate's sales and averageSales (if the associate's sales is lower then the averageSales)
 			}
 		}
